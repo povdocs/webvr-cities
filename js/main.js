4c15201805102c499d94a6c2b47f8a6d0e375dfb
Brian Chirls <brian@chirls.com>
2014-12-12 12:50:26 -0500

    Load Vizicities switchboards from external config files for better organization

--- js/main.js
+++ js/main.js
@@ -291,8 +291,10 @@
 	}
 
 	function initVizi() {
-		var switchboardBuildings,
-			switchboardMap;
+		var layers = [
+			'buildings',
+			'map'
+		];
 
 		viziWorld = new VIZI.World({
 			viewport: document.body,
@@ -307,147 +309,19 @@
 		camera.position.set(0, 0, 0);
 		camera.rotation.set(0, 0, 0);
 
-		switchboardBuildings = new VIZI.BlueprintSwitchboard({
-			input: {
-				type: "BlueprintInputGeoJSON",
-				options: {
-					tilePath: "http://vector.mapzen.com/osm/buildings/{z}/{x}/{y}.json"
-				}
-			},
-			output: {
-				type: "BlueprintOutputBuildingTiles",
-				options: {
-					grids: [{
-						zoom: 15,
-						tilesPerDirection: 2,
-						cullZoom: 13
-					}],
-					workerURL: "js/lib/vizi-worker.js"
-				}
-			},
-			triggers: [{
-				triggerObject: "output",
-				triggerName: "initialised",
-				triggerArguments: ["tiles"],
-				actionObject: "input",
-				actionName: "requestTiles",
-				actionArguments: ["tiles"],
-				actionOutput: {
-					tiles: "tiles" // actionArg: triggerArg
-				}
-			}, {
-				triggerObject: "output",
-				triggerName: "gridUpdated",
-				triggerArguments: ["tiles"],
-				actionObject: "input",
-				actionName: "requestTiles",
-				actionArguments: ["tiles"],
-				actionOutput: {
-					tiles: "tiles" // actionArg: triggerArg
-				}
-			}, {
-				triggerObject: "input",
-				triggerName: "tileReceived",
-				triggerArguments: ["geoJSON", "tile"],
-				actionObject: "output",
-				actionName: "outputBuildingTile",
-				actionArguments: ["buildings", "tile"],
-				actionOutput: {
-					buildings: {
-						process: "map",
-						itemsObject: "geoJSON",
-						itemsProperties: "features",
-						transformation: {
-							outline: "geometry.coordinates",
-							height: "properties.height",
-							min_height: "properties.min_height"
-						}
-					},
-					tile: "tile"
-				}
-			}]
-		});
-		switchboardBuildings.addToWorld(viziWorld);
+		layers.forEach(function (layer) {
+			d3.json('layers/' + layer + '.json', function(error, data) {
+				var switchboard;
 
-		//*
-		switchboardMap = new VIZI.BlueprintSwitchboard({
-			input: {
-				type: "BlueprintInputMapTiles",
-				options: {
-					tilePath: "https://a.tiles.mapbox.com/v3/examples.map-i86l3621/{z}/{x}/{y}@2x.png"
+				if (error) {
+					console.warn(error);
+					return;
 				}
-			},
-			output: {
-				type: "BlueprintOutputImageTiles",
-				options: {
-					grids: [
-						{
-							zoom: 19,
-							tilesPerDirection: 3,
-							cullZoom: 17
-						// }, {
-						// 	zoom: 18,
-						// 	tilesPerDirection: 3,
-						// 	cullZoom: 16
-						}, {
-							zoom: 17,
-							tilesPerDirection: 3,
-							cullZoom: 15
-						// }, {
-						// 	zoom: 16,
-						// 	tilesPerDirection: 3,
-						// 	cullZoom: 14
-						}, {
-							zoom: 15,
-							tilesPerDirection: 3,
-							cullZoom: 13
-						// }, {
-						// 	zoom: 14,
-						// 	tilesPerDirection: 3,
-						// 	cullZoom: 12
-						}, {
-							zoom: 13,
-							tilesPerDirection: 5,
-							cullZoom: 11
-						}
-					]
-				}
-			},
-			triggers: [{
-				triggerObject: "output",
-				triggerName: "initialised",
-				triggerArguments: ["tiles"],
-				actionObject: "input",
-				actionName: "requestTiles",
-				actionArguments: ["tiles"],
-				actionOutput: {
-					tiles: "tiles" // actionArg: triggerArg
-				}
-			}, {
-				triggerObject: "output",
-				triggerName: "gridUpdated",
-				triggerArguments: ["tiles"],
-				actionObject: "input",
-				actionName: "requestTiles",
-				actionArguments: ["tiles"],
-				actionOutput: {
-					tiles: "tiles" // actionArg: triggerArg
-				}
-			}, {
-				triggerObject: "input",
-				triggerName: "tileReceived",
-				triggerArguments: ["image", "tile"],
-				actionObject: "output",
-				actionName: "outputImageTile",
-				actionArguments: ["image", "tile"],
-				actionOutput: {
-					image: "image", // actionArg: triggerArg
-					tile: "tile"
-				}
-			}]
+
+				switchboard = new VIZI.BlueprintSwitchboard(data);
+				switchboard.addToWorld(viziWorld);
+			});
 		});
-		switchboardMap.addToWorld(viziWorld);
-		//c*/
 	}
 
 	function initControls() {
