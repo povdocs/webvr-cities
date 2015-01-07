852d87e30d481d76373c5fe3a0bad5e4057a42fc
Brian Chirls <brian@chirls.com>
2015-01-07 09:51:04 -0500

    Bug fix: particles were moving in the wrong direction when view is flipped - clean out some unnecessary shader code junk - better organization of population data viz objects/code

--- js/dataviz/population.js
+++ js/dataviz/population.js
@@ -1,19 +1,35 @@
-window.dataViz('population', {
-	layers: {
-		population: true
-	},
-	info: [
-		'<h2>Local population by race</h2>',
-		'<p>Source: US Census 2010. Inspired by ',
-		'<a href="http://www.coopercenter.org/demographics/Racial-Dot-Map" target="_new">',
-		'The Racial Dot Map</a>.',
-		'<ul style="list-style: none; padding: 0; margin: 0;">',
-		'<li><span style="color: rgb(115, 178, 255);">&#x2B24;</span> White</li>',
-		'<li><span style="color: rgb(85, 255, 0);">&#x2B24;</span> Black</li>',
-		'<li><span style="color: rgb(255, 0, 0);">&#x2B24;</span> Asian</li>',
-		'<li><span style="color: rgb(255, 170, 0);">&#x2B24;</span> Hispanic</li>',
-		'<li><span style="color: rgb(136, 90, 68);">&#x2B24;</span> Other Race / Native American / Multi-racial</li>',
-		'</ul>'
-	].join(''),
-	height: 30
-});
+window.dataViz('population', (function () {
+	var body;
+
+	return {
+		layers: {
+			population: true
+		},
+		info: [
+			'<h2>Local population by race</h2>',
+			'<p>Source: US Census 2010. Inspired by ',
+			'<a href="http://www.coopercenter.org/demographics/Racial-Dot-Map" target="_new">',
+			'The Racial Dot Map</a>.',
+			'<ul style="list-style: none; padding: 0; margin: 0;">',
+			'<li><span style="color: rgb(115, 178, 255);">&#x2B24;</span> White</li>',
+			'<li><span style="color: rgb(85, 255, 0);">&#x2B24;</span> Black</li>',
+			'<li><span style="color: rgb(255, 0, 0);">&#x2B24;</span> Asian</li>',
+			'<li><span style="color: rgb(255, 170, 0);">&#x2B24;</span> Hispanic</li>',
+			'<li><span style="color: rgb(136, 90, 68);">&#x2B24;</span> Other Race / Native American / Multi-racial</li>',
+			'</ul>'
+		].join(''),
+		init: function (scene) {
+			var i;
+			for (i = 0; i < scene.children.length; i++) {
+				body = scene.children[i];
+				if (body.name === 'body') {
+					break;
+				}
+			}
+		},
+		layersLoaded: function (layers) {
+			body.add(layers.population.object);
+		},
+		height: 30
+	};
+}()));
