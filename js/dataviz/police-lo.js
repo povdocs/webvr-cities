7e0e4b2a626608e584ac9b05ddf062582e9d8515
Brian Chirls <brian@chirls.com>
2015-01-12 18:30:16 -0500

    Two versions of police injury data: high and low

--- js/dataviz/police-lo.js
+++ js/dataviz/police-lo.js
@@ -1,4 +1,4 @@
-window.dataViz('police', {
+window.dataViz('police-lo', {
 	layers: {
 		policeclaims: true
 	},
@@ -8,5 +8,8 @@ window.dataViz('police', {
 		'The Office of the New York Comptroller’s Office. Inspired by ',
 		'<a href="http://iquantny.tumblr.com/post/104986136969/new-yorkers-file-an-average-of-ten-lawsuits-a-day" target="_new">',
 		'I Quant NY</a>.'
-	].join('')
+	].join(''),
+	height: 6,
+	longitude: -73.890541,
+	latitude: 40.863306
 });
