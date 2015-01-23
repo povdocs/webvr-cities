ced7c2174fa6a068d8b8f9bedc794c98356ad3a7
Brian Chirls <brian@chirls.com>
2015-01-23 14:18:44 -0500

    The Department of Redundancy Department

--- js/dataviz/police.js
+++ js/dataviz/police.js
@@ -5,7 +5,7 @@ window.dataViz('police', {
 	info: [
 		'<h2>New York City Personal Injury Police Action Claims 2013</h2>',
 		'<p>Source: <a href="http://comptroller.nyc.gov/reports/claimstat/" target="_new">ClaimStat</a>, ',
-		'The Office of the New York Comptroller’s Office. Inspired by ',
+		'The Office of the New York City Comptroller. Inspired by ',
 		'<a href="http://iquantny.tumblr.com/post/104986136969/new-yorkers-file-an-average-of-ten-lawsuits-a-day" target="_new">',
 		'I Quant NY</a>.'
 	].join('')
